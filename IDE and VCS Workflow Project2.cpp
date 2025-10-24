084390cf86140c12308286b0f558b2f916d1b2df
cryst <cryst@MOMS_PUTER>
2025-10-23 20:11:31 -0400

    Add project files.

---
+++ IDE and VCS Workflow Project2.cpp
@@ -0,0 +1,16 @@
+// IDE and VCS Workflow Project2.cpp 
+// This program displays your initials.
+
+#include <iostream>
+using namespace std;
+int main() {
+    char first, middle, last;
+    //Store your initials in these variables
+    first = 'S';
+    middle = 'F';
+    last = 'R';
+
+    cout << first << middle << last << endl;
+    return 0;
+
+}
